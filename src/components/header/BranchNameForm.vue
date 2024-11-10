4d0c7f553c7e14001e7ae26756ba5d89b430f17b
Javier N. Pérez <javier.perez@chappsolutions.com>
2024-11-10 18:35:14 +0100

    Add header and branch name form

---
+++ src/components/header/BranchNameForm.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { BranchNameFormType } from '@/core/types'
+
+const ticketId = ref('');
+const developName = ref('');
+
+const emit = defineEmits(['submitForm']);
+
+
+const handleSubmit = (event: Event) => {
+  event.preventDefault();
+  emit('submitForm', { ticketId: ticketId.value, developName: developName.value } as BranchNameFormType);
+};
+</script>
+
+<template>
+  <form aria-label="Create branch name form" @submit="handleSubmit">
+    <input type="text" name="ticket-id" aria-label="Ticket name" v-model="ticketId">
+    <input type="text" name="develop-name" aria-label="Develop name" v-model="developName">
+    <button type="submit">Generate</button>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
