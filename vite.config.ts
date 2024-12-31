853a9a5771b81ed179cbcb7e3495b65e4fd3dc39
Javier N. Pérez <javier.perez@chappsolutions.com>
2024-12-31 19:21:45 +0100

    Add base route

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/branch-name-formatter/',
   plugins: [vue()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
