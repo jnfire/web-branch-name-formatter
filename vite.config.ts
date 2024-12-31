5c7d538b34e036c5d48b2580258b91d5ff1df820
Javier N. Pérez <javier.perez@chappsolutions.com>
2024-12-31 19:43:02 +0100

    Update repository name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/branch-name-formatter/',
+  base: '/web-branch-name-formatter/',
   plugins: [vue()],
   resolve: {
     alias: {
